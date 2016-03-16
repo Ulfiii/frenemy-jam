8c04afc9ac5d5eaf7f944600099215c4669f2b23
Andrew Mroczkowski <andy.mroczkowski@gmail.com>
2016-03-16 12:58:20 -0400

    Touch controls & cleanups

---
+++ Assets/Scripts/PlayerBasicMovement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerBasicMovement : MonoBehaviour
+{
+	public string playedId;
+
+	public float speed = 1.0f;
+
+
+	void Start()
+	{
+	}
+
+
+	void Update()
+	{
+		// keyboard
+		var move1 = new Vector3(Input.GetAxis("Horizontal" + playedId), Input.GetAxis("Vertical" + playedId), 0);
+		transform.position += move1 * speed * Time.deltaTime;
+
+		// touch joystick
+		var move2 = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal" + playedId), CrossPlatformInputManager.GetAxis("Vertical" + playedId), 0);
+		transform.position += move2 * speed * Time.deltaTime;
+	}
+}
